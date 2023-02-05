20209079f6cabee8208dd122f1c65082875dc76e
Mateo Rojas <matmomax@gmail.com>
2023-02-04 21:08:26 -0800

    my first c++ project

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    /*
+    this
+    is
+    a
+    comment
+    */
+    std::cout << "Hello World" << '\n';
+    std::cout << "HELLO WORLD!!!!!!!" << '\n';
+    return 0;
+}
